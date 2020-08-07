da7a7cffd5078ff03102fc874e27ce7cf82e0f65
Santha kumar S <s.santhakumar@live.com>
2020-08-06 22:26:21 -0700

    Added Login

---
+++ Quiz/Quiz/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiz.Models;
+
+namespace Quiz.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            Users user = new Users();
+            return View(user);
+        }
+    }
+}
---
+++ Quiz/Quiz/Models/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Quiz.Models
+{
+    [Table("UserDetails")]
+    public class Users
+    {
+        [Required]
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int MobileNumber { get; set; }
+        [Required]
+        public string UserPassword { get; set; }
+        [Required]
+        public string DOB { get; set; }
+        [Required]
+        public string Gender { get; set; }
+    }
+}
